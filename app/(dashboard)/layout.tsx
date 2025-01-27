62c7d7066448e103b77d1a9dc6d8d8bc97646193
arkhanardana <hanzmantap@gmail.com>
2025-01-27 15:23:25 +0700

    add

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Dashboard = ({ children }: { children: React.ReactNode }) => {
    return (
       <>
-         <h1 className='text-3xl'>DASHBOARD</h1>
+         <h1 className='text-3xl'>DASHBOARD NAVBAR</h1>
          <main>
             {children}
          </main>
