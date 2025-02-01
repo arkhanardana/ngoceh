05431c26828a0d699f1dec04dfc56739a1297243
Arkhan Ardana <129038084+arkhanardana@users.noreply.github.com>
2025-02-01 18:41:26 +0700

    Update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
    return (
       <div>
-         <h1 className='text-3xl'>NAVBAR</h1>
+         <h1 className='text-3xl'>NAVBAR BRO</h1>
          {children}
       </div>
    )
